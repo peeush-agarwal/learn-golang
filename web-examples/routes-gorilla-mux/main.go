319d30ca3c70d9611d9269cc1ef219eb1663a037
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-14 18:52:25 +0530

    Tutorial for routing using gorilla/mux

---
+++ web-examples/routes-gorilla-mux/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	port := ":5000"
+	fmt.Printf("Server is running on port %v", port)
+
+	log.Fatal(http.ListenAndServe(port, r))
+}
