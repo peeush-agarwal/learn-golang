1ef531ab440aba06cad89bfc7c6d316e496b0998
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-14 17:59:07 +0530

    hello-world for Go web

---
+++ web-examples/hello-world/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	port := ":5000"
+	fmt.Println("Server is running on port" + port)
+
+	// Start server on port specified above
+	log.Fatal(http.ListenAndServe(port, nil))
+}
