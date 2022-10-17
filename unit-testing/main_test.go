2f0362f5d070579e0d15f8c619cba7fe6001c1fb
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-17 19:29:03 +0530

    Table-driven unit test

--- unit-testing/main_test.go
+++ unit-testing/main_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
-func TestAdd(t *testing.T) {
-	got := add(1, 2)
-	want := 3
+type addTest struct {
+	arg1, arg2, expected int
+}
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+var addTests = []addTest{
+	addTest{1, 2, 3},
+	addTest{2, 3, 5},
+	addTest{-1, -2, -3},
+	addTest{-2, 3, 1},
+}
+
+func TestAdd(t *testing.T) {
+	for _, test := range addTests {
+		got := add(test.arg1, test.arg2)
+		if got != test.expected {
+			t.Errorf("Output %q not equal to expected %q", got, test.expected)
+		}
 	}
 }
