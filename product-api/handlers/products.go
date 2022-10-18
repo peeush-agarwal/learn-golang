0c491a18acff598d510521ddff74ee7919528c7e
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-18 19:20:43 +0530

    Add PUT method handler

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/peeush-agarwal/learn-golang/product-api/data"
 )
@@ -23,6 +25,37 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+
+		re := regexp.MustCompile(`/([0-9]+)`)
+		g := re.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URL more than 1 'id'")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URL more than 1 capture group")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URL Unable to convert to number")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		p.l.Println("Got id", id)
+		p.updateProduct(id, rw, r)
 		return
 	}
 
@@ -55,4 +88,30 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to un-marshal json", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Unable to update product", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.UpdateProduct(id, prod)
 }
