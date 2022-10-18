b6b5fc45613d23eab395534b583e5d3df5bf3b50
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-18 16:20:40 +0530

    Update main for Server creation

--- web-examples/micro-services/main.go
+++ web-examples/micro-services/main.go
@@ -23,34 +23,30 @@ func main() {
 	sm.Handle("/goodbye", gh)
 
 	// create a new server
-	s := http.Server{
+	s := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
-		ErrorLog:     l,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
-		IdleTimeout:  5 * time.Second,
+		IdleTimeout:  120 * time.Second,
 	}
 
 	// start the server
 	go func() {
-		l.Println("Starting the server on port 9090")
-
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Printf("Error starting server: %s\n", err)
-			os.Exit(1)
+			l.Fatal(err)
 		}
 	}()
 
 	// trap sigterm or interrupt signal and gracefully shutdown the server
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
 	// Block until a signal is received
-	sig := <-c
-	log.Println("Got signal:", sig)
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
 
 	// gracefully shutdown the server, waiting max 30 seconds for current operation to complete
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
