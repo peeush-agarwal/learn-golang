ce13c04cc1fb80a2838f7f84fc09f8e0f0ed7fa9
Peeush Agarwal <agarwal.peeush@gmail.com>
2022-10-18 21:35:29 +0530

    Add custom validation on Product.SKU field

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -14,7 +15,7 @@ type Product struct {
 	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price" validate:"gt=0"`
-	SKU         string  `json:"sku"`
+	SKU         string  `json:"sku" validate:"required,sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
@@ -22,7 +23,15 @@ type Product struct {
 
 func (p *Product) Validate() error {
 	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
 	return validate.Struct(p)
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	return len(matches) == 1
 }
 
 func (p *Product) FromJSON(r io.Reader) error {
--- product-api/data/products_test.go
+++ product-api/data/products_test.go
@@ -6,6 +6,7 @@ func TestChecksValidation(t *testing.T) {
 	p := &Product{
 		Name:  "A",
 		Price: 1.0,
+		SKU:   "abc-def-ghi",
 	}
 
 	err := p.Validate()
